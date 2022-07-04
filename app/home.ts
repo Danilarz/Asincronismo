5e3f78ac09c67d7ca7e31384fdba383f3289798c
agustinanahir <agustina.ferreira@live.com>
2022-07-04 18:54:47 -0300

    uso de ts

---
+++ app/home.ts
@@ -0,0 +1,18 @@
+const filterForm = document.getElementById('filter-form');
+const locationsControl = document.getElementById('locations-control');
+
+const loadLocations = async () => {
+    const locations = await getLocations()
+
+    locationsControl.innerHTML = '';
+
+    locations.forEach(loc => {
+        const optionName = document.createElement('option')
+        const txtName = document.createTextNode(loc.name)
+       
+        optionName.appendChild(txtName)
+        
+        locationsControl.appendChild(optionName)
+    })
+    
+}
