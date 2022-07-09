907a0702ee27be414b31dd4607acfaf5fc993ae0
agustinanahir <agustina.ferreira@live.com>
2022-07-09 17:53:39 -0300

    cargamos los select

--- appi/locations.ts
+++ appi/locations.ts
@@ -1,18 +1,19 @@
 const addLocation = async (locations) => {
     const options = {
-      method: 'POST',
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(locations)
     }
     
-    await fetch('https://tp--asincronismo-default-rtdb.firebaseio.com/locations.json', options)
+    await fetch('https://tp--asincronismo-default-rtdb.firebaseio.com/locations/-N5Y77MVh0R5UNrYq5Lp.json', options)
+    
     
 }
 
 const getLocations = async () => {
-  const response = await fetch('https://tp--asincronismo-default-rtdb.firebaseio.com/locations.json')
+  const response = await fetch('https://tp--asincronismo-default-rtdb.firebaseio.com/locations/-N5Y77MVh0R5UNrYq5Lp.json')
   const data = await response.json();  
 
   return mapToArray(data)
